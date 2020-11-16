7d07adbaf3f185e30576afa99ae371a613dce70d
vipul71098 <38537174+vipul71098@users.noreply.github.com>
2020-11-16 07:15:00 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import Input from './component/Input';
 import Display from './component/Display';
-import store from './store/'
 function App() {
   return (
     <div className="App">
       <h1>React Redux Task</h1>
-      <Input store={store} />
+      <Input />
       <Display  />
     </div>
   );
